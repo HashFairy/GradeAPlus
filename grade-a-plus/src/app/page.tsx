cb601bd38940ef52b031f226cab77c940b824724
Kofi Kwakye <kofi.kwakye@live.com>
2025-05-14 07:37:39 +0000

    Setup nextjs and supabase authentication

---
+++ grade-a-plus/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+          <div>
+              <p> Hello world </p>
+          </div>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
